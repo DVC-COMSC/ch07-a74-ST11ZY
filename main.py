0dcc2054202e6a28029740e2d5d1ab9e11e4f5b0
Your Name <you@example.com>
2022-10-11 12:42:57 -0700

    save me

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 
-
 numbers = [5, 20, 30, 30, 50]
 delval = int(input('Enter the deletion value: '))
-
+while True:
+    try:
+        numbers.remove(delval)
+    except: 
+        break   
 # ******************************
 # Make your Code
 # ******************************
